4da3ed2564a8abc0f0c3de2cb9d5f159b3b4639a
Wudong-champlain <143460294+Wudong-champlain@users.noreply.github.com>
2025-09-08 10:51:24 -0400

    Delete 3-stopped-services-to-csv.ps1

--- 3-stopped-services-to-csv.ps1
+++
@@ -1,9 +0,0 @@
-$csv = Join-Path $PSScriptRoot 'stopped-services.csv'
-
-Get-Service |
-  Where-Object Status -eq 'Stopped' |
-  Sort-Object DisplayName |
-  Select-Object Name, DisplayName, Status |
-  Export-Csv -Path $csv -NoTypeInformation
-
-Write-Host "Saved:" $csv
